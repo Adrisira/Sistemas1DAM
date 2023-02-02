df01462397aa8a186a556f0ac5a4ecaa9e156795
Adrián Sigüenza Ramírez <88464152+Adrisira@users.noreply.github.com>
2023-02-02 09:33:43 +0100

    Create ejercicio2.ps1

---
+++ Boletin5/ejercicio2.ps1
@@ -0,0 +1,12 @@
+﻿if ($args.Count -lt 2) {
+    Write-Host "Hay que introducir mas datos"
+} else {
+
+    $mayor = $args[0]
+    for ($index = 0; $index -lt $args.count; $index++){
+        if ($args[$index] -gt $mayor){
+            $mayor = $args[$index]
+        }
+    }
+    Write-Host $mayor
+}
