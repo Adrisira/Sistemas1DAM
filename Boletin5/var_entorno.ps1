e66bc504a831f99de7991b8493de00809e80b5ad
Adrián Sigüenza Ramírez <88464152+Adrisira@users.noreply.github.com>
2023-02-03 09:38:57 +0100

    Create var_entorno

---
+++ Boletin5/var_entorno.ps1
@@ -0,0 +1,12 @@
+﻿if ($args.Count -ne 1){
+    Write-Host "Un solo parametro"
+} elseif ( -not (Test-Path $args[0])){
+    Write-Host "La ruta especificada no existe: " $args[0]
+} else {
+    $env:Path = $env:Path + ";" + $args[0]
+    $ruta = $args[0] + "/*.exe"
+    $ruta2 = $args[0] + "/*.ps1"
+
+    ls $ruta
+    ls $ruta2
+}
