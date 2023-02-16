c59bfd8fe3bd00089b227ebec048bef4e1af5226
Adrián Sigüenza Ramírez <88464152+Adrisira@users.noreply.github.com>
2023-02-16 17:13:23 +0100

    Ej3

---
+++ Boletin3/Ejer2/EJ2.ps1
@@ -0,0 +1,15 @@
+﻿if (Test-Path './Boletin3/' ){
+    rm './Boletin3/' -Recurse
+}
+
+mkdir ./Boletin3/Ejer1 
+mkdir ./Boletin3/Ejer2 
+mkdir ./Boletin3/Ejer3 
+
+copy .\Compara.ps1 ./Boletin3/Ejer1/
+copy .\copia.ps1 ./Boletin3/Ejer2/
+copy .\Compara.ps1 ./Boletin3/Ejer3/
+
+Rename-Item ./Boletin3/Ejer1/Compara.ps1 EJ1.ps1
+Rename-Item ./Boletin3/Ejer2/copia.ps1 EJ2.ps1
+Rename-Item ./Boletin3/Ejer3/Compara.ps1 EJ1.ps1
---
+++ Boletin3/Ejer3/EJ1.ps1
@@ -0,0 +1,13 @@
+﻿if (($args.Count -lt 2) -or ($args.Count -gt 2)){
+    Write-Host "Numero incorrecto de parametros. Hay " $args.Count " y se esperaban 2"
+} else {
+    if ($args[0] -lt $args[1]){
+        Write-Host $args[0] "Es menor que " $args[1]
+    } elseif ($args[0] -eq $args[1]){
+        Write-Host $args[0] "Es igual que " $args[1]
+    } else {
+         Write-Host $args[0] "Es mayor que " $args[1]
+    }
+
+}
+
